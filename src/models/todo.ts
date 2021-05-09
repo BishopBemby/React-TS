898d8882579320972c34295db5ed1d3330278539
Bishop Bemby <bishopbemby1995@gmail.com>
2021-05-09 16:00:35 +0530

    added uuid package and types for ts to work and add unique id for all elemnts in list

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,10 +1,13 @@
+import {v4 as uuidv4} from 'uuid';
 class todo{
    id: string;
    text: string;
 
    constructor(todoText: string){
        this.text = todoText;
-       this.id = Math.floor(Math.random() * 100).toString();
+    //    this.id = Math.floor(Math.random() * 100).toString();
+    // or
+    this.id = uuidv4();
    }
 }
 
